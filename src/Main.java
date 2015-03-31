0409416bf67ed3db5e0a1ea6fc73f94f8d9b626e
Charlie Zhang <peidong97@yahoo.ca>
2015-03-31 10:14:21 -0400

    initialize movie file, write test movies, add moviepanel to the cards panel

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.io.IOException;
 
 import javax.swing.*;
 
@@ -6,25 +7,52 @@ import javax.swing.*;
 public class Main {
 	//Frame
 	public static JFrame frame;
-	public static JPanel mainPanel;
+	public static JPanel cards;
+	public static MovieInfoPanel moviePanel;
+	public static SearchPanel searchPanel;
 	//
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception 
+	{
 		guiApp();
 	}
 	
-	private static void guiApp(){
+	private static void guiApp() throws Exception
+	{		
 		frame = new JFrame("Movie Theatre");
-		mainPanel = new JPanel(new CardLayout());
-		mainPanel.add("1",new SearchPanel());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		cards = new JPanel(new CardLayout());
+		searchPanel = new SearchPanel();
+		moviePanel = new MovieInfoPanel();
+		
+
+		//initialize movie raf (open the file connection)
+		MovieFile.initialize();
+
+		//make up data for an example movie (will use a database later)
+		int[] ironManTimes = {600, 1200, 300, 500};
+		String[] ironManCast = {"Michael Bay", "Bill Cosby", "Matt Damon"};
+		
+		//new movies 
+		MovieRecord movie = new MovieRecord("Iron Man", "It's a cool movie", ironManTimes, ironManCast, 1);
+		MovieRecord movie2 = new MovieRecord("Iron Man 2", "the same movie but the sequel", ironManTimes, ironManCast, 2);
+		MovieRecord movie3 = new MovieRecord("Iron Man 3", "its iron man but with more 3", ironManTimes, ironManCast, 3);
+		
+		//write to movieFile
+		MovieFile.writeRecord(1, movie);
+		MovieFile.writeRecord(2, movie2);
+		MovieFile.writeRecord(3, movie3);
+		
+		cards.add("1",searchPanel);
+		cards.add("2", moviePanel);
 		
 		//Execute frame
-		frame.add(mainPanel);
+		frame.add(cards);
 		frame.pack();
 		frame.setVisible(true);
 	}
-	private static void switchCard(int card){
-		 CardLayout cardLayout = (CardLayout)mainPanel.getLayout();
-	     cardLayout.show(mainPanel,""+card);
+	
+	public static void switchCard(int card){
+		 CardLayout cardLayout = (CardLayout)cards.getLayout();
+	     cardLayout.show(cards,""+card);
 	}
 }
