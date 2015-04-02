a2635face9b891e9510bfc373e5066d7b57744da
Charlie Zhang <peidong97@yahoo.ca>
2015-04-02 13:05:01 -0400

    readded view all panel
    lolwhoopsaccidentallydeleted

--- src/SearchPanel.java
+++ src/SearchPanel.java
@@ -27,9 +27,12 @@ public class SearchPanel extends JPanel{
 	protected JPanel filmsPnl; //Displays results and shows featured films
 	protected JPanel centerPnl; //Contains filmsPnl and the picture
 	protected JLabel searchType;
+	protected JPanel leftPanel;
+	protected JFrame allFrame;
 	//Buttons
 	private static JButton byDateBtn;
 	private static JButton byTitleBtn;
+	private static JButton viewAll;
 	private JButton optionsBtn;
 	private JButton managementBtn;
 	
@@ -58,6 +61,9 @@ public class SearchPanel extends JPanel{
 		searchBox2 = new JList<String>();
 		searchType = new JLabel("Featured");
 		
+		leftPanel = new JPanel(new BorderLayout());
+		viewAll = new JButton("All Movies");
+		
 		byDatePnl.add("1",byDateBtn);
 		byDatePnl.add("2",searchBox2);
 		byTitlePnl.add("1",byTitleBtn);
@@ -75,12 +81,15 @@ public class SearchPanel extends JPanel{
 		ButtonHandler onClick = new ButtonHandler();
 		byDateBtn.addActionListener(onClick);
 		byTitleBtn.addActionListener(onClick);
+		viewAll.addActionListener(onClick);
 		//Adding document Listener for search box
 		SearchHandler onSearch = new SearchHandler();
 		searchBox.getDocument().addDocumentListener(onSearch);
 		
 		//Add to panel
-		centerPnl.add(searchType,BorderLayout.LINE_START);
+		leftPanel.add(searchType, BorderLayout.CENTER);
+		leftPanel.add(viewAll,BorderLayout.PAGE_END);
+		centerPnl.add(leftPanel,BorderLayout.LINE_START);
 		centerPnl.add(new JScrollPane(filmsPnl),BorderLayout.CENTER);
 		searchPnl.add(byDatePnl,BorderLayout.LINE_START);
 		searchPnl.add(byTitlePnl,BorderLayout.CENTER);
@@ -94,7 +103,7 @@ public class SearchPanel extends JPanel{
 	{
 		String query = searchBox.getText();
 		filmsPnl.removeAll();
-		System.out.println("Search Query: " + query);
+		log.v("Search Query: " + query);
 		if(query.equals(""))
 		{
 			searchType.setText("Featured");
@@ -162,11 +171,19 @@ public class SearchPanel extends JPanel{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			Object command = e.getSource();
-			
-			if(command == byDateBtn){
+
+			if(command == byDateBtn)
 				searchMode(1);
-			}else if(command == byTitleBtn)
+			else if(command == byTitleBtn)
 				searchMode(2);
+			else if(command == viewAll)
+				allFrame = new JFrame("All Movies");
+				Container c = allFrame.getContentPane();
+				c.setLayout(new GridLayout((int)Math.ceil(Math.sqrt(records.size())),(int)Math.ceil(records.size()/Math.sqrt(records.size()))));
+				for(int x = 0;x<records.size();x++)
+					allFrame.add(new movieBlock(records.get(x)));
+				allFrame.pack();
+				allFrame.setVisible(true);
 		}
 
 		
@@ -177,7 +194,7 @@ public class SearchPanel extends JPanel{
 		public movieBlock(MovieRecord e) {
 			record = e;
 			setLayout(new BorderLayout());
-			image = new JLabel();
+			image = new JLabel("",JLabel.CENTER);
 			image.setIcon(new ImageIcon(new ImageIcon(Main.class.getResource(record.imageID + ".jpg")).getImage().getScaledInstance(150, 200, Image.SCALE_SMOOTH)));
 			add(image, BorderLayout.CENTER);
 			add(new JLabel(e.getTitle(),JLabel.CENTER),BorderLayout.PAGE_END);
@@ -191,6 +208,7 @@ public class SearchPanel extends JPanel{
 			     cardLayout.show(cards,"2");
 			     MovieInfoPanel moviePanel = (MovieInfoPanel) cards.getComponent(1);
 			     moviePanel.setMovie(record);
+			     allFrame.dispose();
 			}
 			
 		}
