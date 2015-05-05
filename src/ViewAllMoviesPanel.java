66f8ed3d3187dd895a3f034ffae84e9cbb2cd5b3
Charlie Zhang <peidong97@yahoo.ca>
2015-05-04 20:06:40 -0400

    searching by date now selects date, decorate billing panel, scroll faster

--- src/ViewAllMoviesPanel.java
+++ src/ViewAllMoviesPanel.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
